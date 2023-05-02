85c0f5fc5d4da2f4f699e320f9ace068f46ff5fa
BayuuOFC <Bayurizkyananda65@gmail.com>
2023-05-02 17:22:20 +0700

    v2

---
+++ plugins/own-tagall.js
@@ -0,0 +1,18 @@
+import moment from 'moment-timezone'
+let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    let bcbg = 'https://ibb.co/5hRhCD2'
+    conn.send3ButtonLoc(m.chat, bcbg, wm, `${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────','Menu', '.menu', 'Owner', '.owner', `\nKakek Gw Sugiono`, '.huuu', null,
+ /*let m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, */{
+        mentions: users
+    })
+}
+
+handler.help = ['o-tagall']
+handler.tags = ['owner']
+handler.command = ['o-tagall']
+handler.owner = false
+handler.premium = true
+handler.group = true
+handler.register = false
+export default handler
